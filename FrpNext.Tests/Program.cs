1088beaa055123a40ea51d0b95214e642fa5e43d
Sandro Magi <naasking@gmail.com>
2015-07-31 12:09:53 -0400

    -added a simple console test

---
+++ FrpNext.Tests/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace FrpNext.Tests
+{
+    class Program
+    {
+        static Stream<int> Ints(int n)
+        {
+            return Streams.Unfold(i => Tuple.Create(i, Next.Delay(() => i + 1)), n);
+        }
+        static void Run(int k, Stream<int> xs)
+        {
+            while (k-- > 0)
+            {
+                Debug.Assert(15 + 11 - k - 1 == xs.Value);
+                Console.WriteLine(xs.Value);
+                Runtime.Tick();
+                xs = xs.Next.Force();
+            }
+        }
+        static void Main(string[] args)
+        {
+            var stream = Ints(15);
+            Run(11, stream);
+            Console.ReadLine();
+        }
+    }
+}
